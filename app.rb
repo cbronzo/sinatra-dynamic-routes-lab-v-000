c2af6b4f1cc6da13335b58cee053cb19777c4c26
Catherine Bronzo <cbronzo@mac.com>
2018-08-06 23:36:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,7 +21,8 @@ class App < Sinatra::Base
     @num.times do
      puts @phrase
     end
-end
+  end
+  binding.pry
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     @phrase = params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5] + "."
