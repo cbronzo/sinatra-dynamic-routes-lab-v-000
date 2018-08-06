2b552bc1c3ca1eb54aa95e900ad192bfd0afecf1
Catherine Bronzo <cbronzo@mac.com>
2018-08-06 23:44:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,30 +18,6 @@ class App < Sinatra::Base
     @num = params[:number].to_i
     @phrase = params[:phrase]
     "#{@phrase*@num}
-  
-  get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @phrase = params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5] + "."
-    
-    
-  "#{@phrase}"
-  # # end
-  
-  # get '/:operation/:number1/:number2' do
-  #   @operation = params[:operation]
-  #   @num1 = params[:number1].to_i
-  #   @num2 = params[:number2].to_i
-  #   case @operation
-  #     when "add"
-  #       @total = @num1 + @num2
-  #     when "subtract"
-  #       @total = @num1 - @num2
-  #     when "multiply"
-  #       @total = @num1 * @num2
-  #     when "divide"
-  #       @total = @num1 / @num2
-  #   end
-  #   "#{@total}"
-  # end
-    
+
 
 end
