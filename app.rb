e5244d98ce16aaedd848fc40fec4a1f4d4ef5f9b
Catherine Bronzo <cbronzo@mac.com>
2018-08-06 23:40:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,9 +16,7 @@ class App < Sinatra::Base
   
   get '/say/:number/:phrase' do
     @num = params[:number].to_i
-    @phrase = params[:phrase]
-  end
-  binding.pry
+    "#{@number*@number}"
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     @phrase = params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5] + "."
